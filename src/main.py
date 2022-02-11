98da21c2c87bf19b807c31a94a721a43363e9ecf
Alfayed <baksha97@gmail.com>
2022-02-10 19:18:28 -0500

    discord template

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
-import pandas as pd
-df = pd.DataFrame()
-print("Hello, World!")
+from dotenv import load_dotenv, dotenv_values
+import discord
+
+load_dotenv() 
+config = dotenv_values(".env")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(config["BOT_TOKEN"])
